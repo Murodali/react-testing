e73edf5ecbed01d6a250f326cc10695a635a3360
Murodali <smurodali@gmail.com>
2023-06-29 16:18:59 +0500

    check

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to test react
+          Learn React
         </a>
       </header>
     </div>
